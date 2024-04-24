d65392ece556404036a08a958b1ee5cc46a9b0f5
hywax <me@hywax.space>
2024-04-24 19:28:13 +0500

    chore: declare types

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+declare module 'phaser-font-plugin' {
+  export class FontPlugin extends Phaser.Plugins.BasePlugin {
+    constructor(pluginManager: Phaser.Plugins.PluginManager)
+    fontFileCallback(key: string, url: string, xhrSettings?: Phaser.Types.Loader.XHRSettingsObject)
+  }
+
+  export class FontFile extends Phaser.Loader.File {
+    constructor(loader: Phaser.Loader.LoaderPlugin, key: string, url: string, xhrSettings?: Phaser.Types.Loader.XHRSettingsObject)
+    load(): void
+  }
+}
+
+declare namespace Phaser {
+  namespace Loader {
+    interface LoaderPlugin {
+      font(name: string, path: string, xhrSettings?: Phaser.Types.Loader.XHRSettingsObject): this
+    }
+  }
+}
